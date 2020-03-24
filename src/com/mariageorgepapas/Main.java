2615893de37657edaf222a3ac10a12e7acdd21a0
morpheus <mariageorgepapas@gmail.com>
2020-03-23 23:29:35 -0500

    Abbreviating Operators

--- src/com/mariageorgepapas/Main.java
+++ src/com/mariageorgepapas/Main.java
@@ -21,5 +21,35 @@ public class Main {
         result = result % 3;  // the remainder of (4 % 3) result set from line 18
         System.out.println("4 % 3 = " + result);
 
+
+        // Abbreviating Operators
+
+        // result = result + 1;
+        result++;    // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        // result = result - 1;
+        result--;    // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result + 2;
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result - 2;
+        result -=2;  // 3 - 2 = 1
+        System.out.println("3 - 2 = " + result);
+
+        // result = result * 15;
+        result *= 15; // 1 * 15 = 15
+        System.out.println("1 * 15 = " + result);
+
+        // result = result / 3;
+        result /= 3; // 15 / 3 = 5
+        System.out.println("15 / 3 = " + result);
+
+
+
+
     }
 }
