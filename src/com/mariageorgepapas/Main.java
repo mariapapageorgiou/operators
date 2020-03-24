30a2c65d1ea32ffad3fbbf07bf0cca8f34c9924d
morpheus <mariageorgepapas@gmail.com>
2020-03-24 00:11:20 -0500

    Logical and operator &&

--- src/com/mariageorgepapas/Main.java
+++ src/com/mariageorgepapas/Main.java
@@ -53,5 +53,16 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than the second top score and less than 100");
+        }
+
     }
 }
