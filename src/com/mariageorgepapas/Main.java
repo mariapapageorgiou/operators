c99a9260f9b42f4b5715817ff3272ca98dfb39bd
morpheus <mariageorgepapas@gmail.com>
2020-03-27 00:57:41 -0500

    Operators Challenge

--- src/com/mariageorgepapas/Main.java
+++ src/com/mariageorgepapas/Main.java
@@ -83,5 +83,26 @@ public class Main {
         if (wasCar) {
             System.out.println("wasCar is true");
         }
+
+        // Operators Challenge
+        // 1. Create a double variable with a value of 20.00
+        double firstValue = 20.00d;
+        // 2. Create a second variable of type double with the value 80.00
+        double secondValue = 80.00d;
+        // 3. Add both numbers together and multiply by 100.00
+        double totalValue = (firstValue + secondValue) * 100.00;
+        System.out.println("totalValue = " + totalValue);
+        // 4. Use the remainder operator to figure out what the remainder from the result of the operation in step 3 and 40.00 is.
+        double remainder = totalValue % 40.00d;
+        System.out.println("remainder = " + remainder);
+        // 5. Create a boolean variable that assigns the value true if the remainder in #4 is 0, false if it's not zero.
+        boolean isRemainderZero = remainder == 0 ? true : false;
+        // 6. Output the boolean variable.
+        System.out.println("isRemainderZero = " + isRemainderZero);
+        // 7. Write an if-then statement that displays a message "Got some remainder" if the boolean in step 5 is not true.
+        //    Be surprised if you see output for this step, where you might expect it not to show.
+        if (!isRemainderZero) {
+            System.out.println("Got some remainder");
+        }
     }
 }
